373f175448cfcfa89a279654e32d9aa9ac846e54
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-09-06 12:21:46 +0200

    Use standard api

--- modules/lucky.py
+++ modules/lucky.py
@@ -4,10 +4,17 @@ This module gets the lucky number.
 
 import requests
 
-API_URL = "https://zsk-poznan.github.io/szczesliwy-numerek-backend"
+old_API_URL = "https://zsk-poznan.github.io/szczesliwy-numerek-backend"
+API_URL = "https://get-lucky.netlify.app/.netlify/functions/get"
+
+def old_get_lucky():
+    r = requests.get(old_API_URL)
+    r = r.json()
+    return r["TK"]
 
 def get_lucky():
     r = requests.get(API_URL)
     r = r.json()
-    return r["TK"]
-
+    if "data" in r:
+        return r["data"]["luckyNumber"]
+    return 0
