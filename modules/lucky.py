57bd38e9ba2a3ee500ecbda83cfd7e64376aaae4
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-11-10 09:13:36 +0100

    Update lucky.py

--- modules/lucky.py
+++ modules/lucky.py
@@ -4,16 +4,16 @@ This module gets the lucky number.
 
 import requests
 
-old_API_URL = "https://zsk-poznan.github.io/szczesliwy-numerek-backend"
-API_URL = "https://get-lucky.netlify.app/.netlify/functions/get"
-
-def old_get_lucky():
-    r = requests.get(old_API_URL)
-    r = r.json()
-    return r["TK"]
+API_URL = "https://zsk-poznan.github.io/szczesliwy-numerek-backend"
+old_API_URL = "https://get-lucky.netlify.app/.netlify/functions/get"
 
 def get_lucky():
     r = requests.get(API_URL)
+    r = r.json()
+    return r["TK"]
+
+def old_get_lucky():
+    r = requests.get(old_API_URL)
     r = r.json()
     if "data" in r:
         return r["data"]["luckyNumber"]
